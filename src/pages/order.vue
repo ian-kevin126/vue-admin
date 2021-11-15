a8004faf6e8f9e34cbd8ca4cef025b988aea56a0
liaojiayun <liaojiayun@moresales.cn>
2021-11-15 16:23:24 +0800

    feat：开发首页

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <router-view></router-view>
+    <service-bar></service-bar>
+    <nav-footer></nav-footer>
+  </div>
 </template>
-
 <script>
-export default {};
+  import ServiceBar from './../components/ServiceBar'
+  import NavFooter from './../components/NavFooter'
+  export default{
+    name:'order',
+    data(){
+      return {
+        title:'',
+        tip:''
+      }
+    },
+    components:{
+      ServiceBar,
+      NavFooter
+    }
+  }
 </script>
-
-<style></style>
